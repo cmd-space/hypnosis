73e5167a61a03903150bde50e1060fb0688aa05c
Mason Crane <masonecrane@gmail.com>
2015-06-25 17:49:01 -0700

    added bootstrap navbar

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
-require('./config/mongoose.js');
-app.use(bodyParser.json());
-require('./config/routes.js')(app);
+// require('./config/mongoose.js');
+// app.use(bodyParser.json());
+// require('./config/routes.js')(app);
 
 app.use(express.static(path.join(__dirname, './client')));
 
